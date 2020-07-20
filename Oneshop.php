736ae7fcc446fb8e0072d4183fcb68d006a95240
humphreymwangi36 <67624025+humphreymwangi36@users.noreply.github.com>
2020-07-20 11:25:10 +0300

    Oneshop.php

---
+++ Oneshop.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title> Oneshop</title>
+        <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min"/>
+    </head>
+<body>
+        <div class="menu-bar">
+    
+    <ul>
+        <li class="active"><a href="Oneshop.php" >Home</a></li>
+        
+       <li>Televisions
+       <div class="sub-menu-1">
+           <ul>
+               <li><a href="detail1.php">Lg</a></li>
+               <li><a href="#">Saumsung</a></li>
+               <li><a href="#">Digital</a></li>
+           </ul>
+       </div></li>
+
+    
+       <li>Laptops
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Hp</a></li>
+                   <li><a href="#">Dell</a></li>
+                   <li><a href="#">Mini-Laptops</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Watches
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Amref</a></li>
+                   <li><a href="#">Ex-Japan</a></li>
+                   <li><a href="#">Clocker</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Whoofers
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Vitron</a></li>
+                   <li><a href="#">Sony</a></li>
+                   <li><a href="#">Skywall</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Contacts
+           <div class="sub-menu-1">
+               <ul>
+                  <li>0704117507</li>
+                  <li>Kairu33@gmail</li>
+               </ul>
+           </div>
+         </li>   
+         </ul>
+    </div>
+    <div id="backgroung-image">
+        <div id="image-1"></div>
+            <div id="image-2"></div>
+    </div>    
+    <div class="footer" > Copyright @Oneshop 2020</footer></div>
+     
+ </body>
+   
+ </html>
+
+
+
---
+++ detail1.php
@@ -0,0 +1,114 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title> Details1 </title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min"/>
+    </head>
+<body>
+        <div class="menu-bar">
+    
+    <ul>
+        <li class="active"><a href="Oneshop.php" >Home</a></li>
+        
+       <li>Televisions
+       <div class="sub-menu-1">
+           <ul>
+               <li><a href="# ">Lg</a></li>
+               <li><a href="#">Saumsung</a></li>
+               <li><a href="#">Digital</a></li>
+           </ul>
+       </div></li>
+
+    
+       <li>Laptops
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Hp</a></li>
+                   <li><a href="#">Dell</a></li>
+                   <li><a href="#">Mini-Laptops</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Watches
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Amref</a></li>
+                   <li><a href="#">Ex-Japan</a></li>
+                   <li><a href="#">Clocker</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Whoofers
+           <div class="sub-menu-1">
+               <ul>
+                   <li><a href="#">Vitron</a></li>
+                   <li><a href="#">Sony</a></li>
+                   <li><a href="#">Skywall</a></li>
+               </ul>
+           </div>
+        </li>
+        <li>Contacts
+           <div class="sub-menu-1">
+               <ul>
+                  <li>0704117507</li>
+                  <li>Kairu33@gmail</li>
+               </ul>
+           </div>
+         </li>   
+         </ul>
+    </div>
+    <div class="class-1">
+        <div> 
+<p>
+    <h1>
+Firts at this point
+</h1>
+</p>
+    </div>
+
+    <div class="class-2">
+        
+<p>
+    <h>
+Brake them down
+</h>
+</p>
+
+    </div>
+
+    <div class="class-3">
+        
+<p>
+Bring it up
+</p>
+        
+        
+    </div>
+
+    <div class="class-4">
+        
+<p>
+
+WEb plus
+   </p>     
+        
+    </div>
+
+    <div class="class-5">
+        
+     <p>
+
+nnnshdm nbjmd kmnmnm sd xknn m
+       </p> 
+    </div>
+</div>
+    <div class="footer" > Copyright @Oneshop 2020</footer></div>
+     
+ </body>
+   
+ </html>
+
+
+
